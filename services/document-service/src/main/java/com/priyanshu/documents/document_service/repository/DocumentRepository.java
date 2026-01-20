24710c483f9e08c7618c162e48d706027faed184
21Priyanshu <priyanshusingh0872@gmail.com>
2026-01-20 09:34:01 +0530

    added download service

---
+++ services/document-service/src/main/java/com/priyanshu/documents/document_service/repository/DocumentRepository.java
@@ -0,0 +1,11 @@
+package com.priyanshu.documents.document_service.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.priyanshu.documents.document_service.entity.Document;
+
+public interface DocumentRepository extends JpaRepository<Document, UUID> {
+}
+
